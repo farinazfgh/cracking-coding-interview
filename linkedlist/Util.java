bf190cf7264cedeea7804db24d658c174112bdba
farinazfgh <farinaz.ghasemi@gmail.com>
2020-09-05 17:54:43 +0200

    add, delete print linkedlist

--- linkedlist/Util.java
+++ linkedlist/Util.java
@@ -1,2 +1,24 @@
-package linkedlist;public class Util {
+package linkedlist;
+
+public class Util {
+    static Node add(Node head, int data) {
+        if (head == null) {
+            head = new Node(data);
+            return head;
+        }
+        Node pointer = head;
+        while (pointer.next != null) {
+            pointer = pointer.next;
+        }
+        pointer.next = new Node(data);
+        return pointer.next;
+    }
+   static void visit(Node node) {
+       if (node == null) return;
+       while (node != null) {
+           System.out.print(node.data + " ");
+           node = node.next;
+       }
+       System.out.println();
+   }
 }
