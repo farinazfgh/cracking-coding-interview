39a233673ec5f924abf731d891e0097272b7932f
farinazfgh <farinaz.ghasemi@gmail.com>
2020-09-05 11:45:24 +0200

    simple add

--- linkedlist/LinkedListG.java
+++ linkedlist/LinkedListG.java
@@ -2,27 +2,47 @@ package linkedlist;
 
 public class LinkedListG {
     Node head = null;
+    int size = 0;
 
     void add(int data) {
         if (head == null) {
-            head = new Node(0);
+            head = new Node(data);
+            return;
         }
+        Node pointer = head;
+        while (pointer.next != null) {
+            pointer = pointer.next;
+        }
+        pointer.next = new Node(data);
+        size++;
     }
 
     void delete(int data) {
         if (head == null) return;
+
         if (head.data == data) {
-            Node del = head;
             head = head.next;
-            del = null;//avoid lothering
+            size--;
+            return;
         }
+
         Node pointer = head;
         while (pointer.next != null) {
             if (pointer.next.data == data) {
-
                 pointer.next = pointer.next.next;
+                size--;
+                return;
             }
             pointer = pointer.next;
         }
+    }
+
+    void visit(Node node) {
+        if (node == null) return;
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
     }
 }
