bc7c482909470602fd874d11187e86163663aa46
farinazfgh <farinaz.ghasemi@gmail.com>
2020-09-27 13:56:37 +0200

    A[i]=i

---
+++ recursiondynamicprogramming/MagicIndex.java
@@ -0,0 +1,11 @@
+package recursiondynamicprogramming;
+/*
+Magic Index: A magic index in an array A [1. .. n -1] is defined to be an index such that
+A[i]=i
+Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in
+array A.
+What if the values are not distinct?
+ */
+
+public class MagicIndex {
+}
