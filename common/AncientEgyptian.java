de77f351585175f30cc0e5eb334d342cfde4cc1f
Farinaz <farinaz.ghasemi@gmail.com>
2021-01-16 11:36:09 +0100

    implementing the multiply fucntion using >> and << in logarithmic time instead of linear

--- common/AncientEgyptian.java
+++ common/AncientEgyptian.java
@@ -1,13 +1,19 @@
 package common;
 
 public class AncientEgyptian {
-    static int func(int a, int b) {
-        int x = a, y = b, z = 0;
+    static int multiply(int a, int b) {
+        int x = a, y = b, sum = 0;
         while (x > 0) {
-            if (x % 2 == 1) z = z + y;//add only if odd
+            if (x % 2 == 1) sum = sum + y;//add only if odd
             y <<= 1;//y=y*2
             x >>= 1;//x=x/2;
         }
-        return z;
+        return sum;
+    }
+
+    int multiplyRecursive(int a, int b) {
+        if (a == 0) return 0;
+        if (a % 2 == 0) return (2 * multiplyRecursive(a / 2, b));
+        return b + 2 * multiplyRecursive((a - 1) / 2, b);
     }
 }
