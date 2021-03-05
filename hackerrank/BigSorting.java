0b6e76833c55afbcc21b7a84d219b1cfd64609fc
Farinaz Ghasemi <farinaz.ghasemi@gmail.com>
2021-03-05 12:41:47 +0330

    Time limit exceeded

---
+++ hackerrank/BigSorting.java
@@ -0,0 +1,13 @@
+package hackerrank;
+
+import java.util.Arrays;
+
+public class BigSorting {
+    static String[] bigSorting(String[] unsorted) {
+        Arrays.sort(unsorted, (s1, s2) -> {
+            if (s1.length() == s2.length()) return s1.compareTo(s2);
+            return (s1.length() - s2.length());
+        });
+        return unsorted;
+    }
+}
