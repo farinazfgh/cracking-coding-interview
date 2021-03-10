b9a8deafdc3ff464073bf67359b518e45eabd3a7
Farinaz Ghasemi <farinaz.ghasemi@gmail.com>
2021-03-10 06:44:07 +0330

    calculates the ratios of its elements that are positive, negative, and zero

---
+++ warmup/PlusMinus.java
@@ -0,0 +1,26 @@
+package warmup;
+
+public class PlusMinus {
+    static void printResult(double p, double n, double z) {
+        System.out.println(p);
+        System.out.println(n);
+        System.out.println(z);
+    }
+
+    // Complete the plusMinus function below.
+    static void plusMinus(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            printResult(0.0, 0.0, 0.0);
+            return;
+        }
+        double positives = 0.0;
+        double negatives = 0.0;
+        double zeros = 0.0;
+        for (int value : arr) {
+            if (value > 0) positives++;
+            else if (value < 0) negatives++;
+            else zeros++;
+        }
+        printResult(positives / arr.length, negatives / arr.length, zeros / arr.length);
+    }
+}
