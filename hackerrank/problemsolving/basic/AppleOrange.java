c6bad1e6d0f80d057f04c83af223a381d6b0594e
Farinaz Ghasemi <farinaz.ghasemi@gmail.com>
2021-03-21 08:53:43 +0330

    x, y axis negative distance start from 0

--- hackerrank/problemsolving/basic/AppleOrange.java
+++ hackerrank/problemsolving/basic/AppleOrange.java
@@ -1,2 +1,22 @@
-package hackerrank.problemsolving.basic;public class AppleOrange {
+package hackerrank.problemsolving.basic;
+
+public class AppleOrange {
+    // Complete the countApplesAndOranges function below.
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples
+            , int[] oranges) {
+        //   0-------a----s------------t----------b
+        int applesno = 0;
+        int orangesno = 0;
+
+        for (int distanceFromTree : apples) {
+            int distance = a + distanceFromTree;
+            if (distance >= s && distance <= t) applesno++;
+        }
+        for (int distanceFromTree : oranges) {
+            int distance = b + distanceFromTree;
+            if (distance >= s && distance <= t) orangesno++;
+        }
+        System.out.println(applesno);
+        System.out.println(orangesno);
+    }
 }
